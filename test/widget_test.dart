2829b46aee2bf14c450d295c5015980a14c3115e
João Pedro Machado de Oliveira <joaopedromachadodeoliveira10@gmail.com>
2026-07-20 11:20:52 -0300

    test: atualizar widget test para refletir nova estrutura do app

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:servico_facil/main.dart';
+import '../lib/main.dart';
 import 'package:shimmer/main.dart';
 
 void main() {
